7adb9fa1204064db1ee70d53b8757b580ecb6a08
Sam Littlefair <sam.littlefair@prismic.io>
2020-09-23 14:53:44 +0200

    Upgrade Nuxt to 2.14, remove  from nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,51 +1,60 @@
-const pkg = require('./package')
+const pkg = require("./package");
 
 export default {
-  mode: 'universal',
-  target: 'static',
+  mode: "universal",
+  target: "static",
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
     title: pkg.name,
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: pkg.description },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Lato:400,700,900,400italic,700italic' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Lora:400,400italic,700,700italic' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons' },
-      { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.3.0/css/flag-icon.min.css'}
-    ]
+      { rel: "icon", type: "image/x-icon", href: "/favicon.png" },
+      {
+        rel: "stylesheet",
+        href:
+          "https://fonts.googleapis.com/css?family=Lato:400,700,900,400italic,700italic",
+      },
+      {
+        rel: "stylesheet",
+        href:
+          "https://fonts.googleapis.com/css?family=Lora:400,400italic,700,700italic",
+      },
+      {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/icon?family=Material+Icons",
+      },
+      {
+        rel: "stylesheet",
+        href:
+          "https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.3.0/css/flag-icon.min.css",
+      },
+    ],
   },
 
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#fff" },
 
   /*
-  ** Global CSS
-  */
-  css: [
-    '@/assets/css/resetr.css',
-    '@/assets/css/common.css'
-  ],
+   ** Global CSS
+   */
+  css: ["@/assets/css/resetr.css", "@/assets/css/common.css"],
 
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
   /*
-  ** Nuxt.js modules
-  */
-  modules: [
-    '@nuxtjs/prismic'
-  ],
+   ** Nuxt.js modules
+   */
+  modules: ["@nuxtjs/prismic"],
 
   prismic: {
     endpoint: "https://your-repo-name.cdn.prismic.io/api/v2",
@@ -53,19 +62,19 @@ export default {
   },
 
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
+     ** You can extend webpack config here
+     */
     extend(config, ctx) {
       // to transform link with <nuxt-link> for the htmlSerializer
-      config.resolve.alias['vue'] = 'vue/dist/vue.common'
-    }
+      config.resolve.alias["vue"] = "vue/dist/vue.common";
+    },
   },
-  
+
   generate: {
-    fallback: '404.html' // Netlify reads a 404.html, Nuxt will load as an SPA
-  }
-}
+    fallback: "404.html", // Netlify reads a 404.html, Nuxt will load as an SPA
+  },
+};
