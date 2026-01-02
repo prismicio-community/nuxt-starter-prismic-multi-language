acb53ffe77f228f109fafe880a61159785a7f87f
lihbr <lihbr@users.noreply.github.com>
2026-01-02 13:59:05 +0900

    fix: `useAsyncData` watchers

--- app/composables/useNavigation.ts
+++ app/composables/useNavigation.ts
@@ -2,7 +2,7 @@ export const useNavigation = () => {
   const { locale } = useI18n()
   const { client } = usePrismic()
   return useAsyncData(
-    "$navigation",
+    `$navigation-${locale.value}`,
     () => client.getSingle('navigation', { lang: locale.value }),
     { watch: [locale] }
   ).data
--- app/composables/useSettings.ts
+++ app/composables/useSettings.ts
@@ -2,7 +2,7 @@ export const useSettings = () => {
   const { locale } = useI18n()
   const { client } = usePrismic()
   return useAsyncData(
-    "$setttings",
+    `$settings-${locale.value}`,
     () => client.getSingle('settings', { lang: locale.value }),
     { watch: [locale] }
   ).data
